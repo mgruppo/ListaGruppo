c01516f72aa59b97fb1754f3758a5e57d7853cc2
mgruppo <99413368+mgruppo@users.noreply.github.com>
2023-03-28 11:43:05 -0300

    se controla el proyecto, se envia para su revisión por parte del tutor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,9 +9,9 @@ import { Alumno } from 'src/app/models';
 export class HomeComponent {
   public alumnos: Alumno[] = [
     new Alumno(1, 'Gruppo', 'Mariano', 'mgruppo@mail.com', 37, new Date('1985-11-26'),true),
-    new Alumno(3, 'Joe', 'Doe', 'jdoe@mail.com', 35, new Date('1987-05-14'),false),
-    new Alumno(4, 'Juan', 'Perez', 'kakashi@mail.com', 23, new Date('2000-02-24'),false),
-    new Alumno(5, 'Paulo', 'Dybala', 'pdybala@mail.com', 29, new Date('1993-11-15'),true),
-    new Alumno(6, 'Lionel', 'Messi', 'lmessi@mail.com', 35, new Date('1987-06-24'),true),
+    new Alumno(2, 'Joe', 'Doe', 'jdoe@mail.com', 35, new Date('1987-05-14'),false),
+    new Alumno(3, 'Juan', 'Perez', 'kakashi@mail.com', 23, new Date('2000-02-24'),false),
+    new Alumno(4, 'Paulo', 'Dybala', 'pdybala@mail.com', 29, new Date('1993-11-15'),true),
+    new Alumno(5, 'Lionel', 'Messi', 'lmessi@mail.com', 35, new Date('1987-06-24'),true),
   ];
 }
